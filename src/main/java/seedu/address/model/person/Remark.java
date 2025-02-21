a3b79983cf0ae291be8344c396eb09ca19d27cca
KarKuen <karkuenkakkoii@gmail.com>
2025-02-21 19:42:46 +0800

    Improve coding standard

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Instantiate a remark
+     * @param remark The value of remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
